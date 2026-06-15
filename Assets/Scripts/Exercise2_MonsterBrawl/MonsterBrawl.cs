5c03a09085eab45f7611524d64cb869f8db715ff
Plutraser <plutraser@gmail.com>
2026-06-15 16:40:34 -0500

    Added monster randomization and fighting sim
    Added the first and second monster randomization as well as implementing the combat code. Now all I have to do is figure out how to make each monster fight each other once.

--- Assets/Scripts/Exercise2_MonsterBrawl/MonsterBrawl.cs
+++ Assets/Scripts/Exercise2_MonsterBrawl/MonsterBrawl.cs
@@ -31,31 +31,112 @@ using UnityEngine;
 
 public class MonsterBrawl : MonoBehaviour
 {
+    public int turnCount = 0;
+
     struct MonsterData
     {
         public string monsterName;
         public int attackStats;
         public int healthStats;
         public int speedStats; // Speed means each time it attacks each round. EX. A monster with speed 2 attacks on turns 2, 4, 6
+        public bool isDead;
+        public bool isWinner;
 
     }
-    void Start()
-    {
-        MonsterData[] monsters =
+
+    MonsterData[] monsters =
         {
-           new MonsterData { monsterName = "Goblin", attackStats = 8, healthStats = 30, speedStats = 1},
-           new MonsterData { monsterName = "Orc", attackStats = 20, healthStats = 80, speedStats = 2},
-           new MonsterData { monsterName = "Troll", attackStats = 35, healthStats = 200, speedStats = 3},
-           new MonsterData { monsterName = "Skeleton", attackStats = 12, healthStats = 50, speedStats = 1},
-           new MonsterData { monsterName = "Ogre", attackStats = 50, healthStats = 250, speedStats = 4}
+           new MonsterData { monsterName = "Goblin", attackStats = 8, healthStats = 30, speedStats = 1, isDead = false, isWinner = false},
+           new MonsterData { monsterName = "Orc", attackStats = 20, healthStats = 80, speedStats = 2, isDead = false, isWinner = false},
+           new MonsterData { monsterName = "Troll", attackStats = 35, healthStats = 200, speedStats = 3, isDead = false, isWinner = false},
+           new MonsterData { monsterName = "Skeleton", attackStats = 12, healthStats = 50, speedStats = 1, isDead = false, isWinner = false},
+           new MonsterData { monsterName = "Ogre", attackStats = 50, healthStats = 250, speedStats = 4, isDead = false, isWinner = false}
 
         };
 
+    void Start()
+    {
+        PrintingMonsterNames();
+        FightingSim();
+        Debug.Log("All monsters have fought.");
+       
+
+        
+        //Debug.Log(monsters[randomNumber].monsterName);
+    }
+
+    void PrintingMonsterNames()
+    {
         foreach (MonsterData m in monsters)
         {
             Debug.Log(m.monsterName + " | HP:" + m.healthStats + " | ATK:" + m.attackStats + " | SPD:" + m.speedStats);
         }
+    }
 
+    void FightingSim()
+    {
+        int firstMonster = Random.Range(0, monsters.Length);
+        int secondMonster = Random.Range(0, monsters.Length);
+
+        
+        while (firstMonster == secondMonster) //This cycles the second monster to make sure it isnt the same as the first monster
+        {
+            secondMonster = Random.Range(0, monsters.Length);
+        }
+
+        
+
+        while (monsters[firstMonster].isDead == false && monsters[secondMonster].isDead == false) //This loop will go until one monster dies
+        {
+            turnCount += 1;
+            Debug.Log(turnCount);
+
+            if (turnCount % monsters[firstMonster].speedStats == 0) //This modulo checks if the monsters speed is multiple of the turn so it can attack
+            {
+                Debug.Log(monsters[firstMonster].monsterName + " attacks for " + monsters[firstMonster].attackStats);
+                
+                monsters[secondMonster].healthStats -= monsters[firstMonster].attackStats;
+
+                Debug.Log(monsters[secondMonster].monsterName + " has " + monsters[secondMonster].healthStats + " HP remaining");
+            }
+
+            if (turnCount % monsters[secondMonster].speedStats == 0) //Checks for the second monster as well
+            {
+
+                Debug.Log(monsters[secondMonster].monsterName + " attacks for " + monsters[secondMonster].attackStats);
+                monsters[firstMonster].healthStats -= monsters[secondMonster].attackStats;
+            }
+
+            if (monsters[firstMonster].healthStats <= 0 && monsters[secondMonster].healthStats <= 0) //Checks if both died
+            {
+                monsters[firstMonster].isDead = true;
+                monsters[secondMonster].isDead = true;
+                DisplayDrawResults(monsters[firstMonster].monsterName, monsters[secondMonster].monsterName, turnCount);
+            }
+            else if (monsters[firstMonster].healthStats <= 0) 
+            {
+                monsters[firstMonster].isDead = true;
+                monsters[secondMonster].isWinner = true;
+                DisplayResults(monsters[firstMonster].monsterName, monsters[secondMonster].monsterName, turnCount, monsters[secondMonster].healthStats);
+            }
+            else if (monsters[secondMonster].healthStats <= 0)
+            {
+                monsters[secondMonster].isDead = true;
+                monsters[firstMonster].isWinner = true;
+                DisplayResults(monsters[firstMonster].monsterName, monsters[secondMonster].monsterName, turnCount, monsters[firstMonster].healthStats);
+            }
+
+
+        }
+
+    }
+    void DisplayDrawResults(string firstName, string secondName, int turns)
+    {
+        Debug.Log(firstName + " vs " + secondName + " | Draw | Turns: " + turns);
+    }
+    void DisplayResults(string loser, string winner, int turns, int HP)
+    {
+        Debug.Log(loser + " vs " + winner + " | Winner: " + winner + " | Turns: " + turns + " | Remaining HP: " + HP);
     }
 
 }
